d9c335bf7f5261bb030f1d33e3a30eb3c7c52584
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-03-13 01:19:56 +1100

    Updated on load method and header border styling

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,48 +1,53 @@
 const lightModeEl = document.querySelector('.lightmode-button');
 const darkModeEl = document.querySelector('.darkmode-button');
-const toggleButtonEl = document.querySelector('.toggle-button');
 const headerContainer = document.querySelector('.header-container');
-const backButtonEl =document.querySelector('.back-button');
+const backButtonEl = document.querySelector('.back-button');
 
-const pageTheme = localStorage.getItem('theme');
 
-if(pageTheme == null){
-    localStorage.setItem('theme', 'light');
+
+// Function to refresh the local storage and set the default theme to the page.
+function loadFromLocalStorage() {
+    localStorage.clear();
+
+    let pageTheme = localStorage.getItem('theme');
+    console.log(pageTheme);
+
+    if (pageTheme == null) {
+
+        localStorage.setItem('theme', 'light');
+    }
+
 }
 
 
 // Attach event listener to light mode button element
 lightModeEl.addEventListener('click', function () {
     console.log("In event listerner of light");
-    headerContainer.classList.add('darkMode');  
+    headerContainer.classList.add('darkMode');
 
-    lightModeEl.setAttribute('style','display: none');    
-    darkModeEl.setAttribute('style','display: inline')
-    if(backButtonEl != null){
-    backButtonEl.classList.add('darkMode');
-}
-
-    window.localStorage.setItem('theme','dark');
-  });
-
-
-  darkModeEl.addEventListener('click', function () { 
-    headerContainer.classList.remove('darkMode');   
-    darkModeEl.setAttribute('style','display: none');
-    lightModeEl.setAttribute('style','display: inline');
-    if(backButtonEl != null){
-    backButtonEl.classList.remove('darkMode');
+    lightModeEl.setAttribute('style', 'display: none');
+    darkModeEl.setAttribute('style', 'display: inline')
+    if (backButtonEl != null) {
+        backButtonEl.classList.add('darkMode');
     }
-  });
+
+    window.localStorage.setItem('theme', 'dark');
+});
+
+// Attach event listener to dark mode button element
+darkModeEl.addEventListener('click', function () {
+    headerContainer.classList.remove('darkMode');
+    darkModeEl.setAttribute('style', 'display: none');
+    lightModeEl.setAttribute('style', 'display: inline');
+    if (backButtonEl != null) {
+        backButtonEl.classList.remove('darkMode');
+    }
+});
+
+// Load existing data from local storage on page load
+window.onload = loadFromLocalStorage;
 
 
-//   toggleButtonEl.addEventListener('click', function () {
-//     console.log("In event listerner of light");
-//     backButtonEl.classList.add('darkMode');
-//     localStorage.setItem('theme', dark);
-
-    
-//   });
 
 
-  
+
