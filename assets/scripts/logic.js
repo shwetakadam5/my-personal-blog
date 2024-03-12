e0444d54686884a8ff9112ae3fe3a26154abcf8e
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-03-12 17:33:21 +1100

    Updated the header content and the dark/light theme initial contents

---
+++ assets/scripts/logic.js
@@ -0,0 +1,44 @@
+const lightModeEl = document.querySelector('.lightmode-button');
+const darkModeEl = document.querySelector('.darkmode-button');
+const toggleButtonEl = document.querySelector('.toggle-button');
+const headerContainer = document.querySelector('.header-container');
+const backButtonEl =document.querySelector('.back-button');
+
+const pageTheme = localStorage.getItem('theme');
+
+if(pageTheme == null){
+    localStorage.setItem('theme', light);
+}
+
+
+// Attach event listener to light mode button element
+lightModeEl.addEventListener('click', function () {
+    console.log("In event listerner of light");
+    headerContainer.classList.add('darkMode');  
+
+    lightModeEl.setAttribute('style','display: none');    
+    darkModeEl.setAttribute('style','display: inline')
+    backButtonEl.classList.add('darkMode');
+
+    //window.localStorage.setItem("theme", "light");
+  });
+
+
+  darkModeEl.addEventListener('click', function () { 
+    headerContainer.classList.remove('darkMode');   
+    darkModeEl.setAttribute('style','display: none');
+    lightModeEl.setAttribute('style','display: inline');
+    backButtonEl.classList.remove('darkMode');
+  });
+
+
+  toggleButtonEl.addEventListener('click', function () {
+    console.log("In event listerner of light");
+    backButtonEl.classList.add('darkMode');
+    localStorage.setItem('theme', dark);
+
+    
+  });
+
+
+  
