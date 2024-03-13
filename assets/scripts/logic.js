1725d4acedc0dba61060e033b2cd79239a16adcd
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-03-13 19:11:48 +1100

    Updated the dark/light mode to the form/side sections

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,9 +1,11 @@
 const lightModeEl = document.querySelector('.lightmode-button');
 const darkModeEl = document.querySelector('.darkmode-button');
-const headerContainer = document.querySelector('.header-container');
 const backButtonEl = document.querySelector('.back-button');
-
-
+const submitButtonEl = document.querySelector('.submit-button');
+const headerContainer = document.querySelector('.header-container');
+const asideContainer = document.querySelector('.side-container');
+const formContainer = document.querySelector('.form-section');
+const bodyContainer = document.querySelector('.body-container');
 
 // Function to refresh the local storage and set the default theme to the page.
 function loadFromLocalStorage() {
@@ -24,12 +26,17 @@ function loadFromLocalStorage() {
 lightModeEl.addEventListener('click', function () {
     console.log("In event listerner of light");
     headerContainer.classList.add('darkMode');
-
+    asideContainer.classList.add('darkMode');
+    formContainer.classList.add('darkMode');
+    bodyContainer.classList.add('darkMode');
+    submitButtonEl.classList.add('darkMode');
     lightModeEl.setAttribute('style', 'display: none');
     darkModeEl.setAttribute('style', 'display: inline')
     if (backButtonEl != null) {
         backButtonEl.classList.add('darkMode');
     }
+
+
 
     window.localStorage.setItem('theme', 'dark');
 });
@@ -37,6 +44,10 @@ lightModeEl.addEventListener('click', function () {
 // Attach event listener to dark mode button element
 darkModeEl.addEventListener('click', function () {
     headerContainer.classList.remove('darkMode');
+    asideContainer.classList.remove('darkMode');
+    formContainer.classList.remove('darkMode');
+    bodyContainer.classList.remove('darkMode');
+    submitButtonEl.classList.remove('darkMode');
     darkModeEl.setAttribute('style', 'display: none');
     lightModeEl.setAttribute('style', 'display: inline');
     if (backButtonEl != null) {
