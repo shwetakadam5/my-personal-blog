c7f440a85476a0b21c7fd2ccfabedc3e2b2ac590
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-03-14 18:19:33 +1100

    updated the comments

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -1,69 +1,69 @@
+// Selects element by id from the form
 const userName = document.querySelector('#user-name');
 const blogTitle = document.querySelector('#blog-title');
 const blogContent = document.querySelector('#blog-content');
 const submitFormButton = document.querySelector('#submit');
 const msgDiv = document.querySelector('#displayMsg');
-// THEN I am presented with a JSON array of blog post objects,
-// each including the post author's username, title of the post, and post's content.
 
+let blogEntriesArr = [];
 
-let blogEntriesArr =[];
-
+//Displays error message
 function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute('class', type);
-  }
-  
-  submitFormButton.addEventListener('click', function (event) {
-    
-    // create user object from submission
+  msgDiv.textContent = message;
+  msgDiv.setAttribute('class', type);
+}
 
-    const storedBLogEnteriesArr = JSON.parse(localStorage.getItem('blogEntries'));
-    
-    if(storedBLogEnteriesArr !=null){      
+// event listener for the submit button on filling up the blog details 
+submitFormButton.addEventListener('click', function (event) {
+
+  //retrieves the user blog enteries from the local storage
+  const storedBLogEnteriesArr = JSON.parse(localStorage.getItem('blogEntries'));
+
+  if (storedBLogEnteriesArr != null) {
     blogEntriesArr = storedBLogEnteriesArr;
-    }
-   
-    const blogDetailsObj = {
+  }
+
+  const blogDetailsObj = {
     username: "",
     blogtitle: "",
     blogcontent: "",
-    };
+  };
 
-    msgDiv.textContent = "";
-    msgDiv.removeAttribute('class');
-  
-    const userInputName = userName.value.trim();
-    const blogInputTitle = blogTitle.value.trim();
-    const blogInputContent =blogContent.value.trim();
-  
-    if (userInputName === '') {
-      displayMessage('error', 'User cannot be blank');
-      event.preventDefault();            
-    } else if (blogInputTitle === '') {
-      displayMessage('error', 'Title cannot be blank');
-      event.preventDefault();     
-    } else if (blogInputContent === '') {
-        displayMessage('error', 'Content cannot be blank');
-        event.preventDefault();       
-    } else {      
-      blogDetailsObj.username = userInputName;
-      blogDetailsObj.blogtitle = blogInputTitle;
-      blogDetailsObj.blogcontent = blogInputContent;
-      blogEntriesArr.push(blogDetailsObj);
-      userName.value = "";
-      blogTitle.value ="";
-      blogContent.value="";
-     
+  msgDiv.textContent = "";
+  msgDiv.removeAttribute('class');
 
-      localStorage.setItem('blogEntries', JSON.stringify(blogEntriesArr));
-           
-    
-    }
-   
-  });
+  const userInputName = userName.value.trim();
+  const blogInputTitle = blogTitle.value.trim();
+  const blogInputContent = blogContent.value.trim();
 
-  function redirectTo(url) {
-     window.location.href = url;
-     return false;
-   }
+  if (userInputName === '') {
+    displayMessage('error', 'User cannot be blank');
+    event.preventDefault();
+  } else if (blogInputTitle === '') {
+    displayMessage('error', 'Title cannot be blank');
+    event.preventDefault();
+  } else if (blogInputContent === '') {
+    displayMessage('error', 'Content cannot be blank');
+    event.preventDefault();
+  } else {
+    // create user object and array from submission
+    blogDetailsObj.username = userInputName;
+    blogDetailsObj.blogtitle = blogInputTitle;
+    blogDetailsObj.blogcontent = blogInputContent;
+    blogEntriesArr.push(blogDetailsObj);
+    userName.value = "";
+    blogTitle.value = "";
+    blogContent.value = "";
+
+    //Sets the user blog enteries into local storage
+    localStorage.setItem('blogEntries', JSON.stringify(blogEntriesArr));
+
+  }
+
+});
+
+// On successful submission the user moves to the blog page
+function redirectTo(url) {
+  window.location.href = url;
+  return false;
+}
